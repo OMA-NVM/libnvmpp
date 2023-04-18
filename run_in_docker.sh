69e0cdab509c92dfd2f5820f9ad0d24f05d49ad7
Christian Hakert <hakert@mars.cs.tu-dortmund.de>
2023-04-18 14:11:34 +0200

    removed sudo from run file

--- run_in_docker.sh
+++ run_in_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo docker build --build-arg UID=$(id -u) --build-arg UNAME=$(id -u -n) --build-arg GID=$(id -g) -t uk_build_cont .
-sudo docker run --privileged -v ${PWD}/.:/uk -it uk_build_cont bash
+docker build --build-arg UID=$(id -u) --build-arg UNAME=$(id -u -n) --build-arg GID=$(id -g) -t uk_build_cont .
+docker run --privileged -v ${PWD}/.:/uk -it uk_build_cont bash
